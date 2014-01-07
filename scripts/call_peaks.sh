a152f8ffd50bbf8a0b8fba1bc542a67006e64e16
afrendeiro <afrendeiro@gmail.com>
2014-01-07 14:32:53 +0100

    finished argument parsing for peak calling script

--- scripts/call_peaks.sh
+++ scripts/call_peaks.sh
@@ -1,11 +1,45 @@
 #!/bin/bash
 
-REFDIR="/home/s3/afr/data/human/E2F7"
-OUTDIR="/home/s3/afr/output/human/E2F7"
+usage() {
+	echo "Usage: $0 -c <ChIP file> -i <input file> [<outputdir>]"
+	echo "-c	ChIP sample file in bed format"
+	echo "-i	Input control file in bed format"
+	echo "-o	OPTIONAL: Directory for output files. Will be"
+	echo "		created if does not exist. Default: current directory"
+	exit
+}
 
-peakzilla.py ${REFDIR}/GSM810993_E2F7_Hela.081213.bwa.bed ${REFDIR}/GSM810994_Input_Hela.081213.bwa.bed > ${OUTDIR}/E2F7_Hela.peaks.tsv
-cut -f 1,2,3 E2F7_Hela.peaks.tsv | tail -n +2 > ${OUTDIR}/E2F7_Hela.peaks.bed
+if [ $# -lt 2 ] ; then
+	echo "You must specify at least 2 arguments."
+	echo 
+	usage
+	exit 1
+fi
 
-# compare peak sets
+#Process the arguments
+    # options that require arguments have a : in front of them
+while getopts hi:o:g: opt
+do
+    case "$opt" in
+        h)  usage;;
+		c)  CHIP="`readlink -e $OPTARG`";;
+        i)  INPUT="`readlink -e $OPTARG`";;
+        \?) usage;;
+   esac
+done
 
+# Capture additional output directory argument if given, if not set to current dir
+shift $(($OPTIND - 1))
+if [[ $1 ]]; then
+    OUTDIR="`readlink -f $1`"
+    mkdir -p $OUTDIR
+else
+    OUTDIR="`readlink -f .`"
+fi
+
+BASENAME=`basename $CHIP .bed`
+
+
+peakzilla.py $CHIP $INPUT > ${OUTDIR}/$BASENAME.peaks.tsv
+cut -f 1,2,3 ${OUTDIR}/$BASENAME.peaks.tsv | tail -n +2 > ${OUTDIR}/$BASENAME.peaks.bed
 
